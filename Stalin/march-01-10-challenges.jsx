517198b7325e2c56617446919d082cdc012003ef
stalin <stalintouch1983@gmail.com>
2018-03-16 09:58:33 -0400

    Update march-01-10-challenges.jsx

--- Stalin/march-01-10-challenges.jsx
+++ Stalin/march-01-10-challenges.jsx
@@ -157,24 +157,264 @@ class TypesOfFood extends React.Component {
 
 //11 ################################
 
+class Fruits extends React.Component {
+	constructor(props) {
+	  super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+		    { /* change code below this line */ }
+				<NonCitrus />
+        <Citrus />
+	   	  { /* change code above this line */ }
+      </div>
+		);
+	}
+};
+
+class TypesOfFood extends React.Component {
+	constructor(props) {
+	 	super(props);
+	}
+  render() {
+  	return (
+    	<div>
+			  <h1>Types of Food:</h1>
+		    { /* change code below this line */ }
+				<Fruits />
+		    { /* change code above this line */ }
+		    <Vegetables />
+    	</div>
+  	);
+	}
+};
+
 //12 ################################
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+  	super(props);
+  }
+  render() {
+    return (
+	    <div>
+		    <h1>Types of Food:</h1>
+        {/* change code below this line */}
+				<Fruits />
+        <Vegetables />
+		    {/* change code above this line */}
+	    </div>
+    );
+  }
+};
+
+// change code below this line
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
+
 
 //13 ################################
 
+
+// change code below this line
+class MyComponent extends React.Component {
+
+  render(){
+    return(
+    	<div><h1>My First React Component!</h1></div>
+  	)
+  }
+}
+
+ReactDOM.render(<MyComponent/>, document.getElementById('challenge-node'))
+
+
 //14 ################################
+
+
+const CurrentDate = (props) => {
+	return (
+		<div>
+			{ /* change code below this line */ }
+			<p>The current date is: {props.date} </p>
+			{ /* change code above this line */ }
+		</div>
+	);
+};
+
+class Calendar extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<h3>What date is it?</h3>
+				{ /* change code below this line */ }
+				<CurrentDate date={Date()} />
+				{ /* change code above this line */ }
+			</div>
+		);
+	}
+};
 
 //15 ################################
 
+const List= (props) => {
+	{ /* change code below this line */ }
+	return <p>{props.tasks.join(", ")}</p>
+	{ /* change code above this line */ }
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+  	super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+				<h2>Today</h2>
+				{ /* change code below this line */ }
+        <List tasks={["grapes", 'mangoes', 'unkown']} />
+				<h2>Tomorrow</h2>
+        <List tasks={['mandarine', 'papaya', 'orange']}/>
+				{ /* change code above this line */ }
+      </div>
+    );
+  }
+};
+
 //16 ################################
+
+const ShoppingCart = (props) => {
+  return (
+		<div>
+			<h1>Shopping Cart Component</h1>
+		</div>
+  )
+};
+// change code below this line
+ShoppingCart.defaultProps = {items: 0}
+
 
 //17 ################################
 
+const Items = (props) => {
+	return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
+
+Items.defaultProps = {
+	quantity: 0
+}
+
+class ShoppingCart extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+  render() {
+    { /* change code below this line */ }
+    return <Items quantity={10} />
+    { /* change code above this line */ }
+  }
+};
+
+
 //18 ################################
+
+const Items = (props) => {
+	return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+
+// change code below this line
+
+// change code above this line
+
+Items.defaultProps = {
+	quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+  render() {
+    return <Items />
+  }
+};
+
+Items.propTypes = {quantity: PropTypes.number.isRequired}
+
 
 //19 ################################
 
+
+class ReturnTempPassword extends React.Component {
+  constructor(props) {
+  	super(props);
+
+  }
+  render() {
+    return (
+        <div>
+        		{ /* change code below this line */ }
+            <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
+            { /* change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class ResetPassword extends React.Component {
+  constructor(props) {
+  	super(props);
+
+  }
+  render() {
+    return (
+        <div>
+        	<h2>Reset Password</h2>
+        	<h3>We've generated a new temporary password for you.</h3>
+        	<h3>Please reset this password from your account settings ASAP.</h3>
+        	{ /* change code below this line */ }
+					<ReturnTempPassword tempPassword={"this is the new password"} />
+        	{ /* change code above this line */ }
+        </div>
+    );
+  }
+};
+
+
+
+
 //20 ################################
 
+
+const Camper = (props) =>{
+
+    return(
+    	<p>{props.name}</p>
+    )
+
+}
+
+Camper.defaultProps = {name: "CamperBot"}
+Camper.propTypes = {name: PropTypes.string.isRequired}
+
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// change code below this line
 //21 ################################
 
 //22 ################################
