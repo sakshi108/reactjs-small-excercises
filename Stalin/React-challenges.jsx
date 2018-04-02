99e50f9d4a0a7ba8554148734af0f9fdca017668
stalin <stalintouch1983@gmail.com>
2018-04-02 09:05:39 -0400

    Update and rename march-01-10-challenges.jsx to React-challenges.jsx

--- Stalin/React-challenges.jsx
+++ Stalin/React-challenges.jsx
@@ -414,7 +414,266 @@ class CampSite extends React.Component {
     );
   }
 };
-// change code below this line
+
 //21 ################################
 
+
+class StatefulComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		// initialize state here
+		this.state ={
+      name: ""
+    }
+	}
+  render() {
+    return (
+	    <div>
+	    	<h1>{this.state.name}</h1>
+	    </div>
+    );
+  }
+};
+
 //22 ################################
+
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: 'Free Code Camp'
+		}
+	}
+  render() {
+    return (
+	    <div>
+				{ /* change code below this line */ }
+				<h1>{this.state.name}</h1>
+	    	{ /* change code above this line */ }
+	    </div>
+    );
+  }
+};
+
+//23 ################################
+
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: 'Free Code Camp'
+		}
+	}
+	render() {
+		// change code below this line
+		const { name } = this.state;
+	  // change code above this line
+  	return (
+	    <div>
+	    	{ /* change code below this line */ }
+        <h1>{name}</h1>
+	    	{ /* change code above this line */ }
+	    </div>
+    );
+  }
+};
+
+//24 ################################
+
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: 'Initial State'
+		};
+		this.click = this.click.bind(this);
+	}
+	click() {
+		// change code below this line
+		this.setState({name: 'React Rocks!'})
+	  // change code above this line
+	}
+	render() {
+  	return (
+	    <div>
+        <button onClick = {this.click}>Click Me</button>
+        <h1>{this.state.name}</h1>
+	    </div>
+    );
+  }
+};
+
+//25 ################################
+
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			itemCount: 0
+		};
+		// change code below this line
+
+		// change code above this line
+	}
+
+	render() {
+  	return (
+	    <div>
+	    	{ /* change code below this line */ }
+        <button onClick={e => this.setState({itemCount: this.state.itemCount+1})} >Click Me</button>
+        { /* change code above this line */ }
+        <h1>Current Item Count: {this.state.itemCount}</h1>
+	    </div>
+    );
+  }
+};
+
+//26 ################################
+
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: 'Hello!'
+		};
+	}
+	setMessage = () => {
+		this.setState({
+			message: 'Goodbye!'
+		});
+	}
+	render() {
+  	return (
+	    <div>
+        <button onClick = {this.setMessage}>Click Me</button>
+        <h1>{this.state.message}</h1>
+	    </div>
+    );
+  }
+};
+
+//27 ################################
+
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			visibility: false
+		};
+	}
+	// change code below this line
+	toggleVisibility = () => {
+    this.setState({visibility: !this.state.visibility})
+  }
+	// change code above this line
+	render() {
+		if (this.state.visibility) {
+	  	return (
+		    <div>
+	        <button onClick = {this.toggleVisibility}>Click Me</button>
+	        <h1>Now you see me!</h1>
+		    </div>
+	    );
+  	} else {
+  		return (
+		    <div>
+	        <button onClick = {this.toggleVisibility}>Click Me</button>
+		    </div>
+	    );
+  	}
+  }
+};
+
+//28 ################################
+
+class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0
+		};
+	}
+	// change code below this line
+	increment = () => {this.setState({count: this.state.count+1})};
+  decrement = () => {this.setState({count: this.state.count -1})};
+	reset = () => {this.setState({count:0})}
+	// change code above this line
+	render() {
+  	return (
+	    <div>
+        <button className='inc' onClick={this.increment}>Increment!</button>
+        <button className='dec' onClick={this.decrement}>Decrement!</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
+	    </div>
+    );
+  }
+};
+
+//29 ################################
+
+class ControlledInput extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			input: ''
+		};
+	}
+	// change code below this line
+
+	// change code above this line
+	render() {
+  	return (
+	    <div>
+        { /* change code below this line */}
+				<input onChange={e => this.setState({input: e.target.value})} />
+      	{ /* change code above this line */}
+        <p>Input: {this.state.input}</p>
+	    </div>
+    );
+  }
+};
+
+//30 ################################
+
+class MyForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			input: '',
+			submit: ''
+		};
+	}
+	handleChange = (event) => {
+		this.setState({
+			input: event.target.value
+		});
+	}
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.setState({
+			submit: this.state.input
+		});
+	}
+	render() {
+  	return (
+  		<div>
+		   	<form onSubmit={this.handleSubmit}>
+	        <input
+	        	value={this.state.input}
+	        	onChange={this.handleChange} />
+	        <button type='submit'>Submit!</button>
+		    </form>
+				<h1>{this.state.submit}</h1>
+		  </div>
+    );
+  }
+};
+
+//31 ################################
+
+//32 ################################
+
+//33 ################################
+
+//34 ################################
